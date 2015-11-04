faf8cd5419e77bef57593ac9a04de0f12f610286
Rob Lowe <rob@lowefrequency.com>
2015-11-04 18:53:41 -0500

    Added general structure for clockselector, the chip that controls which clock is used

---
+++ clockselector.vhd
@@ -0,0 +1,19 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+--clock selector is just a fancy counter that counts to ten. But I am already buying one SIPO shift register and a MUX that has 4 ports,
+--so lets do it this way, for fun
+
+entity clockSelector is
+    port(
+        signal TGL      : in std_logic;
+        signal CLK      :
+        );
+end clockSelector
+
+architecture Struct of clockSelector is
+    
+begin
+
+end Struct;
+
